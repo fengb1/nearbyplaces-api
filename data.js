84d92d5c3eb19bcd4b266f6e78e40a8dd7712360
fengb1 <fengb1@email.arizona.edu>
2020-12-16 09:27:44 -0700

    server.js updated

---
+++ data.js
@@ -0,0 +1,5 @@
+var places = [{name: "Default Place", city: "Tucson", state: "Arizona",
+description: "This is a default place.", id: 1,
+reviews:[{text: 'very good!', author: 'Anonymous'}]}];
+
+module.export.places = places;
--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const express = require("express");
 var cors = require("cors");
+var bodyParser = require('body-parser');
+const db = require('./db');
 
 const app = express();
 
 const port = process.env.PORT || 3002;
 
+var id = 1;
+
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  response.send("Welcome to API");
+  response.send("Welcome to Nearbyplaces API");
+})
+
+app.get('/place', (req, res) => {
+    let name = req.body.name;
+    let city = req.body.city;
+    let state = req.body.state;
+    let description = req.body.description;
+    let id = id;
+    id += 1;
+    db.setPlace(name, city, state, description, id).then(x => response.json(x));
+})
+
+app.get('/places', (req, res) => {
+    db.getPlaces().then(x => response.json(x));
+})
+
+app.get('/review/:placeId', (req, res) => {
+    let id = req.body.id;
+    let author = req.body.author;
+    let text = req.body.text;
+    db.addReview(id, text, author).then(x => response.json({message: "Added review"}));
+})
+
+app.get('/search', (req, res) => {
+    let keyword = req.body.keyword;
+    db.search(keyword).then(x => response.json(x));
 })
 
 app.listen(port, () => {
